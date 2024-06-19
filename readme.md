01044e880be205bc6a334615d5b1cc8da90bca16
A-gamE-programmer <kevinclangner@gmail.com>
2024-06-20 00:32:13 +0800

    Finish basic features
    Todo: Javascript api

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Really simple script that allows you to template your html using `<template>` tags
+See demo.html for usage
+
+Cool stack idea: template-html + Alpine.js + htmx + picocss + vanilla
--- template-html.js
+++ template-html.js
@@ -1,3 +1,58 @@
+/** Change this to false to prevent template substitution
+ *  Change to a promise and template substitution will
+ *  take place after the promise completes
+ */
+let processDOM = true;
+
+/** Replace the target element with the template,
+ *  filling out the template according to the target
+ */
+const insertTemplate =
+(target, template) =>
+{
+    template
+        .querySelectorAll("[data-attributes]")
+        .forEach(elm => {
+            let passThruAttr = elm.dataset.attributes;
+            if (passThruAttr) {
+                passThruAttr
+                    .split(' ')
+                    .forEach(attr => {
+                        const attrVal = target.getAttribute(attr);
+                        if (attrVal || attrVal === "") {
+                            elm.setAttribute(attr, attrVal);
+                        }
+                    });
+            } else {
+                for (const attr of target.attributes)
+                {
+                    if (attr.name == "data-from") { continue };
+                    elm.setAttribute(attr.name, attr.value);
+                }
+            }
+            elm.removeAttribute('data-attributes');
+        });
+    template
+        .querySelectorAll("[data-children-clone]")
+        .forEach(elm => {
+            target
+                .childNodes
+                .forEach(child => {
+                    elm.appendChild(child.cloneNode(true));
+                });
+        });
+    template
+        .querySelectorAll("[data-children]")
+        .forEach(elm => {
+            let l = target.childNodes.length;
+            for (;l > 0; l--) {
+                elm.appendChild(target.childNodes[0]);
+            }
+            elm.removeAttribute("data-children");
+        })
+    target.replaceWith(template);
+}
+
 /** Look for templates and insert them where they should go
  * @param {HTMLElement} element
  */
@@ -21,47 +76,53 @@ const processTemplatedHtml =
             this.templates.set(elementID, docFrag);
         },
     }
+
     element
         .querySelectorAll("template")
         .forEach(elm => {
-            templateHolder.set(elm.id, elm.content.cloneNode(true));
-        })
-    element
-        .querySelectorAll("[data-from]")
-        .forEach(customElement => {
-            const template = templateHolder.getNew(customElement.dataset.from);
-            template
-                .querySelectorAll("[data-attributes]")
-                .forEach(elm => {
-                    let passThruAttr = elm.dataset.attributes;
-                    if (passThruAttr) {
-                        passThruAttr
-                            .split(' ')
-                            .forEach(attr => {
-                                const attrVal = customElement.getAttribute(attr);
-                                if (attrVal || attrVal === "") {
-                                    elm.setAttribute(attr, attrVal);
-                                }
-                            });
-                    } else {
-                        for (const attr of customElement.attributes)
-                        {
-                            if (attr.name == "data-from") { continue };
-                            elm.setAttribute(attr.name, attr.value);
-                        }
-                    }
-                    elm.removeAttribute('data-attributes');
-                });
-            template
-                .querySelectorAll("[data-children]")
-                .forEach(elm => {
-                    customElement
-                        .childNodes
-                        .forEach(child => {
-                            elm.appendChild(child);
-                        })
-                    elm.removeAttribute("data-children");
-                })
-            customElement.replaceWith(template);
+            templateHolder.set(elm.id, elm.content);
         });
+
+    const fromElements = element.querySelectorAll("[data-from]")
+    for (const ctmElm of fromElements) {
+        const template = templateHolder.getNew(ctmElm.dataset.from);
+        insertTemplate(ctmElm, template);
+    }
+
+    const customElementTemplates = element.querySelectorAll("[data-custom-element]")
+    for (const ctmElmTpl of customElementTemplates) {
+        if (ctmElmTpl.dataset.customElement == "replace") {
+            element
+                .querySelectorAll(ctmElmTpl.id)
+                .forEach(elm => insertTemplate(elm, ctmElmTpl.content.cloneNode(true)))
+        } else {
+            const eName = ctmElmTpl.id
+            customElements.define(eName, class extends HTMLElement {});
+            const copyAttrs = [];
+            for (const attr of ctmElmTpl.attributes) {
+                if (attr.name == "data-custom-element") continue;
+                if (attr.name == "id") continue;
+                copyAttrs.push(attr);
+            }
+            element
+                .querySelectorAll(eName)
+                .forEach(elm => {
+                    copyAttrs.forEach(attr => elm.setAttribute(attr.name, attr.value));
+                });
+        }
+    }
 }
+
+document.addEventListener(
+    "DOMContentLoaded", () => { 
+        if (!processDOM) return;
+        if (typeof processDOM == "boolean") {
+            processTemplatedHtml(document.documentElement);
+        }
+        try {
+            processDOM.then(() => processTemplatedHtml(document.documentElement));
+        } catch (e) {
+            console.error(e);
+        }
+    } 
+);
